f2881a18d48835db5facfd23cdce3da822b2e849
Mathis Kretz <mathis@bespinian.io>
2021-09-13 10:51:43 +0200

    Add CLI and Terraform instructions to level-1

---
+++ level-1/advanced/terraform/main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.48.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
+
+  required_version = "~> 1.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "random_pet" "lambda_bucket_name" {
+  prefix = "bespinian-serverless-workshop"
+  length = 4
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = random_pet.lambda_bucket_name.id
+
+  acl           = "private"
+  force_destroy = true
+}
+
+data "archive_file" "lambda_my_function" {
+  type = "zip"
+
+  source_dir  = "${path.module}/../../function"
+  output_path = "${path.module}/function.zip"
+}
+
+resource "aws_s3_bucket_object" "lambda_my_function" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "function.zip"
+  source = data.archive_file.lambda_my_function.output_path
+
+  etag = filemd5(data.archive_file.lambda_my_function.output_path)
+}
+
+resource "aws_lambda_function" "my_function" {
+  function_name = "my-function-logged-terraform-${var.aws_user}"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_bucket_object.lambda_my_function.key
+
+  runtime = "nodejs14.x"
+  handler = "index.handler"
+
+  source_code_hash = data.archive_file.lambda_my_function.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "my_function" {
+  name = "/aws/lambda/${aws_lambda_function.my_function.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless-lambda-${var.aws_user}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
