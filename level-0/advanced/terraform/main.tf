78f53f95c4946a8b7226d8cbca10e0cdcd8d8ad2
Mathis Kretz <mathis@bespinian.io>
2021-09-15 22:06:20 +0200

    Remove s3 bucket from terraform module level-0

--- level-0/advanced/terraform/main.tf
+++ level-0/advanced/terraform/main.tf
@@ -21,18 +21,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "random_pet" "lambda_bucket_name" {
-  prefix = "bespinian-serverless-workshop"
-  length = 4
-}
-
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = random_pet.lambda_bucket_name.id
-
-  acl           = "private"
-  force_destroy = true
-}
-
 data "archive_file" "lambda_my_function" {
   type = "zip"
 
@@ -40,20 +28,10 @@ data "archive_file" "lambda_my_function" {
   output_path = "${path.module}/function.zip"
 }
 
-resource "aws_s3_bucket_object" "lambda_my_function" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-
-  key    = "function.zip"
-  source = data.archive_file.lambda_my_function.output_path
-
-  etag = filemd5(data.archive_file.lambda_my_function.output_path)
-}
-
 resource "aws_lambda_function" "my_function" {
   function_name = "my-function-terraform-${var.aws_user}"
 
-  s3_bucket = aws_s3_bucket.lambda_bucket.id
-  s3_key    = aws_s3_bucket_object.lambda_my_function.key
+  filename = "${path.module}/function.zip"
 
   runtime = "nodejs14.x"
   handler = "index.handler"
