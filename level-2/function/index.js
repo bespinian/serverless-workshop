89862c20a3638a889cbc9b32464351003ef96cd6
Gabriel Koch <7779684+elessar-ch@users.noreply.github.com>
2021-09-18 16:43:10 +0200

    Add support for table suffix through env variable

--- level-2/function/index.js
+++ level-2/function/index.js
@@ -4,8 +4,11 @@ const AWS = AWSXRay.captureAWS(require("aws-sdk"));
 const ddb = new AWS.DynamoDB();
 
 exports.handler = async (event) => {
+  const tableSuffix = process.env.JOKE_TABLE_SUFFIX
+    ? process.env.JOKE_TABLE_SUFFIX
+    : "";
   const params = {
-    TableName: "Jokes",
+    TableName: "Jokes" + tableSuffix,
     Key: { ID: { N: event.jokeID } },
   };
 
--- level-3/function/index.js
+++ level-3/function/index.js
@@ -1,23 +1,44 @@
 const AWSXRay = require("aws-xray-sdk");
 const AWS = AWSXRay.captureAWS(require("aws-sdk"));
 
-const TIMEOUT_GRACE_PERIOD_IN_MILLIS = 1000;
+const TIMEOUT_GRACE_PERIOD_IN_MILLIS = 980;
 
 const ddb = new AWS.DynamoDB();
 
 exports.handler = async (event, context) => {
-  AWS.config.update({
-    httpOptions: {
-      timeout:
-        context.getRemainingTimeInMillis() - TIMEOUT_GRACE_PERIOD_IN_MILLIS,
-    },
-  });
-
+  const tableSuffix = process.env.JOKE_TABLE_SUFFIX
+    ? process.env.JOKE_TABLE_SUFFIX
+    : "";
   const params = {
-    TableName: "Jokes-gk",
+    TableName: "Jokes" + tableSuffix,
     Key: { ID: { N: event.jokeID } },
   };
 
-  const response = await ddb.getItem(params).promise();
-  return response.Item;
+  // Abort before function times out.
+  const timoutPromise = new Promise((_, reject) =>
+    setTimeout(
+      () => reject(new Error("Timeout")),
+      context.getRemainingTimeInMillis() - TIMEOUT_GRACE_PERIOD_IN_MILLIS
+    )
+  );
+
+  responsePromise = ddb.getItem(params).promise();
+
+  try {
+    response = await Promise.race([timoutPromise, responsePromise]);
+    console.log(
+      `Remaining time after db query is ${context.getRemainingTimeInMillis()}ms.`
+    );
+
+    return {
+      joke: response.Item.text.S,
+    };
+  } catch (e) {
+    if (e.message == "Timeout") {
+      return {
+        error: `Query failed to complete within given time. Time remaining: ${context.getRemainingTimeInMillis()}ms.`,
+      };
+    }
+    throw e;
+  }
 };
