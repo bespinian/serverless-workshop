ae5c678283c4e21f5aa5f5d8817a6bf286222361
Lena Fuhrimann <mastertinner@users.noreply.github.com>
2021-09-19 14:38:42 +0200

    Clean up repo

--- level-2/function/index.js
+++ level-2/function/index.js
@@ -2,16 +2,17 @@ const AWSXRay = require("aws-xray-sdk-core");
 const { DynamoDBClient, GetItemCommand } = require("@aws-sdk/client-dynamodb");
 
 exports.handler = async (event) => {
-  const ddb = AWSXRay.captureAWSv3Client(new DynamoDBClient());
-
   const tableSuffix = process.env.JOKE_TABLE_SUFFIX
     ? process.env.JOKE_TABLE_SUFFIX
     : "";
-  const params = {
-    TableName: "Jokes" + tableSuffix,
-    Key: { ID: { N: event.jokeID } },
-  };
 
-  const response = await ddb.send(new GetItemCommand(params));
+  const ddb = AWSXRay.captureAWSv3Client(new DynamoDBClient());
+
+  const cmd = new GetItemCommand({
+    TableName: `Jokes${tableSuffix}`,
+    Key: { ID: { N: event.jokeID } },
+  });
+
+  const response = await ddb.send(cmd);
   return response.Item;
 };
