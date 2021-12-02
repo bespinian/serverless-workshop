b4661f1a203d63a6f2a1dc0a4a4205c909a82788
Lena Fuhrimann <cloudlena@users.noreply.github.com>
2021-12-02 18:00:20 +0100

    Fix typo in joke

--- level-3/advanced/terraform/jokes_table.tf
+++ level-3/advanced/terraform/jokes_table.tf
@@ -17,7 +17,7 @@ resource "aws_dynamodb_table_item" "joke_1" {
   item = <<ITEM
 {
   "ID": {"N": "1"},
-  "Text": {"S": "A biologist, a chemist and a statistician are out hunting, The biologist shoots at a deer and misses five feet to the left. The chemist shoots at the same deer and misses five feet to the right. The statistician shouts, 'We got him!'"}
+  "Text": {"S": "A biologist, a chemist, and a statistician are out hunting. The biologist shoots at a deer and misses five feet to the left. The chemist shoots at the same deer and misses five feet to the right. The statistician shouts, 'We got him!'"}
 }
 ITEM
 }
