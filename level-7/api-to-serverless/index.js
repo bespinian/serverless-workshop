21d75b2174cb83d3b84b0225e7412fbb792d59fb
Mathis Kretz <mkretz@users.noreply.github.com>
2022-04-03 13:32:42 +0200

    Adapt to changes in the way aws forwards requests to lambda

--- level-7/api-to-serverless/index.js
+++ level-7/api-to-serverless/index.js
@@ -1,4 +1,13 @@
 const serverless = require("serverless-http");
-const app = require("./app");
+const app = require("./api.js").app;
 
-exports.handler = serverless(app);
+exports.handler = serverless(app, {
+  request(request, event, context) {
+    if (event.body && event.body.length > 0) {
+      request.body = JSON.parse(event.body); // request coming in via api gateway
+      request.url = "/";
+    } else {
+      request.body = event; // request coming in via lambda test
+    }
+  },
+});
