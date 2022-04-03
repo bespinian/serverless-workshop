10af3f29b0a8d185fca8b9ec506c585951895544
Mathis Kretz <mkretz@users.noreply.github.com>
2022-04-03 10:38:42 +0200

    Fix failing unit test in level 7

--- level-7/unit-tests/index.js
+++ level-7/unit-tests/index.js
@@ -1,12 +1,12 @@
 const { DynamoDBClient, GetItemCommand } = require("@aws-sdk/client-dynamodb");
 
-const tableSuffix = process.env.JOKE_TABLE_SUFFIX
-  ? process.env.JOKE_TABLE_SUFFIX
-  : "";
-
 const ddb = new DynamoDBClient();
 
 exports.handler = async (event) => {
+  const tableSuffix = process.env.JOKE_TABLE_SUFFIX
+    ? process.env.JOKE_TABLE_SUFFIX
+    : "";
+
   const cmd = new GetItemCommand({
     TableName: `jokes${tableSuffix}`,
     Key: { ID: { N: event.jokeID } },
